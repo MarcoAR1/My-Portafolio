8899b93ef79c7d67455124980d25941b9afe6250
MarcoAR1 <marcorivero.mr26@gmail.com>
2021-07-22 06:30:54 -0300

    fixed

--- testthings.js
+++
@@ -1,5 +0,0 @@
-const sortComplete = (a, b) => a[1] === b[1] ? a[0].localeCompare(b[0]) : b[1] - a[1];
-
-
-const name = [['Jone', 200], ['Ara', 700], ['Ara', 100],['tna', 500],['Jone', 200], ['Clara', 500], ['Clara', 100],['tna', 500]]
-console.log(name.sort(sortComplete))
